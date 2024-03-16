3fac897a93eae3e60b95dc19fd0e7bd1f93e101f
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-16 13:32:32 -0700

    Better comments for lowest common ancestors

--- lowest common ancestor.ts
+++ lowest common ancestor.ts
@@ -11,8 +11,8 @@ class TreeNode {
 }
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode, q: TreeNode): TreeNode | null {
-	//Time complexity: O(n)
-	//Space complexity: O(1)
+	//Time complexity: O(n) - we are traversing the tree once
+	//Space complexity: O(1) - constant space with 2 variables
 
 	//get the smaller and larger value of p and q
 	const smallVal: number = Math.min(p.val, q.val);
@@ -21,10 +21,19 @@ function lowestCommonAncestor(root: TreeNode | null, p: TreeNode, q: TreeNode): 
 	//start from root
 	let node: TreeNode | null = root;
 	while (node) {
-		if (node.val >= smallVal && node.val <= largeVal) return node;
+		//if the node value of between the small and large value, then it is the a lowest common ancestor
+		//return the node
+		if (node.val >= smallVal && node.val <= largeVal) 
+			return node;
+		//if the node value is less than the small value and large value, then we start searching
+		//from the right side of the tree
+		//keep searching until we find the lowest common ancestor
 		if (node.val < smallVal && node.val < largeVal) {
 			node = node.right;
 		} else {
+			//if the node value is greater than the small value and large value, then we start searching
+			//from the left side of the tree
+			//keep searching until we find the lowest common ancestor
 			node = node.left;
 		}
 	}
