971d8c17d40a61878a62ae956f0e5c282ebc78db
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-16 15:45:45 -0700

    add fast slow pointer problem of detect cycle in linked list

---
+++ fast slow pointers.ts/detect cycle(loop) in single linked list.ts
@@ -0,0 +1,20 @@
+import type { LinkedListNode } from "../clone linked list with arbitrary pointer";
+
+function hasCycle(head: LinkedListNode | null): boolean {
+    //Complexity:
+    //Time: O(n) - we are traversing the list once
+    //Space: O(1) - constant space with 2 variables
+    
+    if (!head) return false;
+
+    //fast and slow pointer approach
+    //slow move 1, fast move 2
+    let slow = head;
+    let fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow!.next!;
+        fast = fast!.next!.next!;
+        if (slow === fast) return true;
+    }
+    return false;
+}
