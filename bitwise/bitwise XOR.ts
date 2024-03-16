41698bb627e88e47debe5e2762875795915d6b2e
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-16 15:36:04 -0700

    add comments to find single number

--- bitwise/bitwise XOR.ts
+++ bitwise/bitwise XOR.ts
@@ -1,6 +1,9 @@
 function findSingleNumber(nums: number[]): number {
+	//number bitwise XOR itself is always 0
+	//so we can use the XOR operator to find the single number
 	let result = 0;
 	for (let num of nums) {
+		//XOR all the numbers
 		result ^= num;
 	}
 	return result;
