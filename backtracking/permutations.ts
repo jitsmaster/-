8078e49cec21ef1dde395664bf569b4e6ea661dd
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-16 12:29:09 -0700

    Added string permutations with backtracking

--- backtracking/permutations.ts
+++ backtracking/permutations.ts
@@ -27,3 +27,28 @@ export function permute(nums: number[]): number[][] {
 
 	return result;
 }
+
+export function permuteString(str: string) : string[] {
+	//backtracking
+	const result: string[] = [];
+
+	function backtrack(remains: string, permutation: string): void {
+		if (permutation.length === str.length) {
+			result.push(permutation);
+			return;
+		}
+
+		for (let i = 0; i < remains.length; i++) {
+			//step 1, add the first character of remains to the permutation
+			permutation += remains[i];
+			//step 2, recurse with the remaining characters of remains
+			backtrack(remains.slice(0, i) + remains.slice(i + 1), permutation);
+			//step 3, undo step 1, by removing the last character from the permutation
+			permutation = permutation.slice(0, permutation.length - 1);
+		}
+	}
+
+	backtrack(str, '');
+
+	return result;
+}
