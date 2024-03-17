f2e40c23d74659f8f8bd101e5f4bb985b1b9cd88
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-16 18:20:41 -0700

    Update knapsack bu comments

--- Dynamic Programming/knapsack/knapsack-bu.ts
+++ Dynamic Programming/knapsack/knapsack-bu.ts
@@ -8,8 +8,12 @@ function knapsackTabulation(weights: number[], values: number[], capacity: numbe
 	for (let i = 1; i <= n; i++) {
 		for (let j = 1; j <= capacity; j++) {
 			if (weights[i - 1] <= j) {
+				//if the weight of the current item is less than or equal to the remaining capacity
+				//algorithm: max of (current value + value of remaining capacity, value of previous item)
 				dp[i][j] = Math.max(values[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j]);
 			} else {
+				//if the weight of the current item is greater than the remaining capacity
+				//just copy from previous item
 				dp[i][j] = dp[i - 1][j];
 			}
 		}
