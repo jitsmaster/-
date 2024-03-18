9c411a1a405bb895bf8af312a2f3d2e1c088bf38
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-17 17:11:11 -0700

    Update comments for bfs

--- Breadth-first-search.ts
+++ Breadth-first-search.ts
@@ -29,8 +29,8 @@ function breadthFirstSearchWithLevelGrouping(root: TreeNode | null): number[][] 
 	//BFS is using a queue to search breathen first
 	const queue: NodeWithLevel[] = [new NodeWithLevel(root, 0)];
 
-
 	//loop until queue is empty, which means all nodes are visited
+	//the queue is dynamic that entire time
 	while (!!queue.length) {
 		//remove first node from the queue
 		const nodeWithLevel = queue.shift()!;
@@ -47,6 +47,39 @@ function breadthFirstSearchWithLevelGrouping(root: TreeNode | null): number[][] 
 		}
 		if (!!node.right) {
 			queue.push(new NodeWithLevel(node.right, level + 1));
+		}
+	}
+
+	return result; //result is a 2D array, each element is an array of nodes at the same level, no reducing needed
+}
+
+function breadthFirstSearchWithLevelGroupingWithoutSpecialClass(root: TreeNode | null): number[][] {
+	const result: number[][] = [];
+	if (root === null) {
+		return result;
+	}
+
+	//BFS is using a queue to search breathen first
+	const queue: { node: TreeNode; level: number }[] = [{ node: root, level: 0 }];
+
+	//loop until queue is empty, which means all nodes are visited
+	//the queue is dynamic that entire time
+	while (!!queue.length) {
+		//remove first node from the queue
+		const nodeWithLevel = queue.shift()!;
+		const node = nodeWithLevel.node;
+		const level = nodeWithLevel.level;
+		if (!result[level]) {
+			result[level] = [];
+		}
+		result[level].push(node.value);
+
+		//push next level nodes to the queue, with level incremented
+		if (!!node.left) {
+			queue.push({ node: node.left, level: level + 1 });
+		}
+		if (!!node.right) {
+			queue.push({ node: node.right, level: level + 1 });
 		}
 	}
 
