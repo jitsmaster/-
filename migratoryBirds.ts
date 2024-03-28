8f863064729522db32264bc9005559909aab8775
Arnold Wang <arnold.shu.wang@gmail.com>
2024-03-27 20:29:48 -0700

    Update migratory birds to be more efficient

--- migratoryBirds.ts
+++ migratoryBirds.ts
@@ -10,7 +10,7 @@ export function migratoryBirds(arr: number[]): number {
 	//if the count is the same, we can sort by the id ascendingly
 	//then we can return the first element of the sorted map
 
-	//time complexity is O(n + logn) - loop through plus sort
+	//time complexity is O(n) - loop through numbers and map entries (without sort)
 	//space complexity is O(n) - potential largest map size
 	//create a map
 	const map = new Map<number, number>();
@@ -25,16 +25,31 @@ export function migratoryBirds(arr: number[]): number {
 		map.set(n, newCount);
 	}
 
-	//sort the count last
-	const sorted = Array.from(map.entries()).sort((a, b) => {
-		if (a[1] === b[1]) {
-			//if the count is the same, then smaller id trump it
-			return a[0] - b[0];
+	// //sort the count last
+	// const sorted = Array.from(map.entries()).sort((a, b) => {
+	// 	if (a[1] === b[1]) {
+	// 		//if the count is the same, then smaller id trump it
+	// 		return a[0] - b[0];
+	// 	}
+
+	// 	return b[1] - a[1];
+	// });
+
+	// return sorted[0][0];
+
+	//no sorting, just iterate through the map
+	//this will save the sort time of logn, and save the space of n on the sorted array
+	let maxId = 0;
+	let maxCount = 0;
+	for (let [id, count] of map.entries()) {
+		if (count > maxCount) {
+			maxCount = count;
+			maxId = id;
+		} else if (count === maxCount) {
+			maxId = Math.min(maxId, id);
 		}
+	}
 
-		return b[1] - a[1];
-	});
-
-	return sorted[0][0];
+	return maxId;
 
 }
