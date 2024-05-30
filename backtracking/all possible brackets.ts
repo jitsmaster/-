80c2ad1670b4c1532366cbbc9a59a79e685e83c2
Arnold Wang <arnold.shu.wang@gmail.com>
2024-05-29 20:00:33 -0700

    improve space on all possible brackets

--- backtracking/all possible brackets.ts
+++ backtracking/all possible brackets.ts
@@ -1,5 +1,6 @@
 function printBracesCombinations(n: number): void {
 	const result: string[][] = [];
+	const stack: string[] = [];
 
 	//standard backtrack approach
 	//difference is: we have to keep track of the number of open and close brackets
@@ -10,10 +11,10 @@ function printBracesCombinations(n: number): void {
 	//Time: O(2^n) where n is the number of brackets
 	//Space: O(2^n) where n is the number of brackets
 
-	function generateCombinations(open: number, close: number, current: string[]): void {
+	function generateCombinations(open: number, close: number): void {
 		if (open === n && close === n) {
-			result.push([...current]);
-			//really important, must create a copy of the original current,
+			result.push([...stack]);
+			//really important, must create a copy of the original stack,
 			//otherwise, we will be directly manipulating the array pushed in the result
 			//in later iterations. This is a common mistake in backtracking problems
 			return;
@@ -21,21 +22,21 @@ function printBracesCombinations(n: number): void {
 
 		//if open < n, then we can add an open bracket
 		if (open < n) {
-			current.push("{");
-			generateCombinations(open + 1, close, current);
+			stack.push("{");
+			generateCombinations(open + 1, close);
 			//backtrack in case we need to try the other option
-			current.pop();
+			stack.pop();
 		}
 
 		//if close is less than open, then we can add a close bracket
 		if (close < open) {
-			current.push("}");
+			stack.push("}");
 			//backtrack in case we need to try the other option
-			generateCombinations(open, close + 1, current);
-			current.pop();
+			generateCombinations(open, close + 1);
+			stack.pop();
 		}
 	}
 
-	generateCombinations(0, 0, [] as string[]);
+	generateCombinations(0, 0);
 
 }
