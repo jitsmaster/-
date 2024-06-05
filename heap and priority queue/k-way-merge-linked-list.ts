b114efcc60cebab80bd82d9e7177ce77203d8f34
Arnold Wang <arnold.shu.wang@gmail.com>
2024-06-04 17:03:36 -0700

    Update merge 2 sorted list with much faster speed

--- heap and priority queue/k-way-merge-linked-list.ts
+++ heap and priority queue/k-way-merge-linked-list.ts
@@ -39,6 +39,9 @@ function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
 }
 
 function merge2SortedLinkedList(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+	//Analysis: O(n + m) time complexity, since we only advance one list at a time
+	//Space complexity is O(n  + m), new linked linked list holds elements from both list
+
 	//in case either of the list is empty, return the other list
 	if (!l1) return l2;
 	if (!l2) return l1;
@@ -69,3 +72,41 @@ function merge2SortedLinkedList(l1: ListNode | null, l2: ListNode | null): ListN
 
 	return newHeadPrefix.next;
 }
+
+function merge2SortedLinkedListFast(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+	//Analysis: Instead of O(n + m) time complexity, we can achieve O(max(n, m)) time complexity
+	//Space complexity is O(n  + m), new linked linked list holds elements from both list
+
+	//We use list1 as the based of merged list, and insert nodes from list2 into merged
+	//when found list2 node is smaller than list1 node.
+	//This way, the smaller list2 node is inserted infront of the list1 node, and we advancing both merged list and list2 in the same time,
+	//effectily reduce the time on advancing the shorter list.
+
+	//So closer the size of 2 lists, the faster the merge will be
+
+	let preHead = new ListNode(0)
+	let mergedTail = preHead
+	mergedTail.next = list1;
+
+	let otherList = list2;
+
+	while (otherList !== null) {
+		if (mergedTail.next === null || mergedTail.next.val > otherList.val) {
+			//if we find that the other list node is smaller than the current merged list node
+			const newNode = new ListNode(otherList.val);
+			//add the new node infront of the current merged list next node;
+			//the process is:
+			//newNode.next takes the original merged tail next;
+			//mergedTail.next takes the new node (advancing the merged list)
+			//also, otherList advances to the next node
+			//this is the step that saves time, since we are advancing both merged and other list in the same time, 
+			//instead of one after another
+			[newNode.next, mergedTail.next, otherList] = [mergedTail.next, newNode, otherList.next];
+		} else {
+			//if the other list node is not smaller, advance the merged list directly
+			mergedTail = mergedTail.next
+		}
+	}
+
+	return preHead.next
+};
