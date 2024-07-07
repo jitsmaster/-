e925aac0ef674e6f75c68ccf64d3f2ebba1f8753
Arnold Wang <arnold.shu.wang@gmail.com>
2024-07-07 14:34:51 -0700

    Added problemd description of word dictionary

--- Graph/Tree/Trie/Word Dictionary.ts
+++ Graph/Tree/Trie/Word Dictionary.ts
@@ -1,5 +1,43 @@
 
 /**
+ * Design a data structure that supports adding new words and finding if a string matches any
+ * previously added string.
+ *
+ * Implement the WordDictionary class:
+ *
+ * WordDictionary() Initializes the object.
+ * void addWord(word) Adds word to the data structure, it can be matched later.
+ * bool search(word) Returns true if there is any string in the data structure that matches word
+ * or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ *
+ * Example:
+ *
+ * Input
+ * ["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+ * [[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+ * Output
+ * [null,null,null,null,false,true,true,true]
+ *
+ * Explanation
+ * WordDictionary wordDictionary = new WordDictionary();
+ * wordDictionary.addWord("bad");
+ * wordDictionary.addWord("dad");
+ * wordDictionary.addWord("mad");
+ * wordDictionary.search("pad"); // return False
+ * wordDictionary.search("bad"); // return True
+ * wordDictionary.search(".ad"); // return True
+ * wordDictionary.search("b.."); // return True
+ *
+ * Constraints:
+ *
+ * 1 <= word.length <= 25
+ * word in addWord consists of lowercase English letters.
+ * word in search consist of '.' or lowercase English letters.
+ * There will be at most 2 dots in word for search queries.
+ * At most 104 calls will be made to addWord and search.
+ */
+
+/**
  * This solution uses array of sets to store trie as adjacency list
  * Much faster than actual trie traversal
  */
@@ -7,6 +45,7 @@ class WordDictionary {
 	//Instead of using trie, we can use array of sets to store the words
 	//the index of the array is the length of the word - 1
 	//this will make finding the word much faster
+	//we know the word won't be over 20 characters long, so an array of 25 sets is enough
 	wordsByLength!: Array<Set<string>>;
 
 	constructor() {
