a367f68558a05a9ca9a787ce0094ad66597ffece
Arnold Wang <arnold.shu.wang@gmail.com>
2024-07-09 18:45:39 -0700

    Update permutations solutions improve array permute speed, by removing an unecessary step

--- backtracking/permutations.ts
+++ backtracking/permutations.ts
@@ -1,21 +1,46 @@
-export function permute(nums: number[]): number[][] {
+/**
+ * Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [1,2,3]
+ * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * Example 2:
+ * Input: nums = [0,1]
+ * Output: [[0,1],[1,0]]
+ *
+ * Example 3:
+ * Input: nums = [1]
+ * Output: [[1]]
+ *
+ * Constraints:
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * All the integers of nums are unique.
+ *
+ * @param nums - The array of distinct integers
+ * @returns - All possible permutations of the array
+ */
+function permute(nums: number[]): number[][] {
+	//Complexity: 
+	//Time: O(n!), since we have n! permutations
+	//Space: O(n), recursion stack, and the result array
+
 	const result: number[][] = [];
 
-	function backtrack(remainingOptions: number[], permutation: number[]): void {
+	function recurseWithBacktrack(remainingOptions: number[], permutation: number[]): void {
+		//break out condition, length of permutation is the same as the length of the input numbers
+		//means all numbers are being used
 		if (permutation.length === nums.length) {
 			result.push([...permutation]);
 			return;
 		}
 
 		for (let i = 0; i < remainingOptions.length; i++) {
-			if (permutation.includes(remainingOptions[i])) {
-				continue;
-			}
-
 			//step 1 push the current option to the permutation
 			permutation.push(remainingOptions[i]);
 			//step 2 recurse with the remaining options, except the current option
-			backtrack(remainingOptions
+			recurseWithBacktrack(remainingOptions
 				.filter((_, idx) => idx !== i), permutation);
 			//step 3 undo the step 1, by popping the last element from the permutation
 			//if this step is reached, means previous recursion all failed, so we need to backtrack
@@ -23,16 +48,16 @@ export function permute(nums: number[]): number[][] {
 		}
 	}
 
-	backtrack(nums, []);
+	recurseWithBacktrack(nums, []);
 
 	return result;
 }
 
-export function permuteString(str: string): string[] {
+function permuteString(str: string): string[] {
 	//backtracking
 	const result: string[] = [];
 
-	function backtrack(remains: string, permutation: string): void {
+	function recurseWithBacktrack(remains: string, permutation: string): void {
 		if (permutation.length === str.length) {
 			result.push(permutation);
 			return;
@@ -42,13 +67,13 @@ export function permuteString(str: string): string[] {
 			//step 1, add the first character of remains to the permutation
 			permutation += remains[i];
 			//step 2, recurse with the remaining characters of remains
-			backtrack(remains.slice(0, i) + remains.slice(i + 1), permutation);
+			recurseWithBacktrack(remains.slice(0, i) + remains.slice(i + 1), permutation);
 			//step 3, undo step 1, by removing the last character from the permutation
 			permutation = permutation.slice(0, permutation.length - 1);
 		}
 	}
 
-	backtrack(str, '');
+	recurseWithBacktrack(str, '');
 
 	return result;
 }
