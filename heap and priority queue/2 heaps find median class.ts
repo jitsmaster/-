c3d87f8595db3313121e68d1a592642b40235841
Arnold Wang <arnold.shu.wang@gmail.com>
2024-08-11 10:32:54 -0700

    fully tested 2 heaps find median

--- heap and priority queue/2 heaps find median class.ts
+++ heap and priority queue/2 heaps find median class.ts
@@ -1,47 +1,58 @@
 import { MaxPriorityQueue, MinPriorityQueue } from "@datastructures-js/priority-queue";
 
+/**
+ * const { PriorityQueue, MaxPriorityQueue, MinPriorityQueue } = require('@datastructures-js/priority-queue');
+ */
+
 class MedianFinder {
-	private maxHeap = new MaxPriorityQueue<number>();
-	private minHeap = new MinPriorityQueue<number>();
+	//The idea is to use two heaps to store the lower half and the higher half of the input stream
+	//and the median will be the top of the heap with the most elements, or the average of the two tops if they have the same size
+	//the reason we are having the maxheap or small and minheap for large, is to get the top value for the median
+	//maxheap top is the biggest value, minheap top is the smallest value
+	//pick or average will be just he median
 
-	constructor() {
-	}
+	//IMPORTANT: the 2 heaps must be same size or differ by 1, so we need to balance them
+	//if the size of the 2 heaps differ by 2, we need to move the top of the bigger heap to the smaller heap
+	//to make sure they differ by 1
+	small = new MaxPriorityQueue<number>();
+	large = new MinPriorityQueue<number>();
 
-	addNum(num: number): void {
-		// Time Complexity: O(logn) - to enqueue the number in the heaps
-		// Space Complexity: O(n) - used to store the elements in the heaps
+	constructor() { }
 
-		// flow: 
-		// 1. if the maxHeap is empty or the number is less than the maxHeap's front, enqueue the number in the maxHeap,
-		// 	  else enqueue the number in the minHeap
-		// reason: we want to maintain the maxHeap as the heap with the smaller elements
-		if (!this.maxHeap.isEmpty() || num >= this.maxHeap.front()) {
-			this.minHeap.enqueue(num);
+	addNum(num: number) {
+		//Complexity:
+		//Time: O(log n), for the heapify operation
+		//Space: O(n), the size of the heap
+
+		// if minheap has no element, add to it first
+		// or if the number if bigger than the minheap top, add to the minheap
+		if (this.large.isEmpty() || num > this.large.front()) {
+			this.large.enqueue(num);
 		} else {
-			this.maxHeap.enqueue(num);
+			this.small.enqueue(num);
 		}
 
-		// 2. if the maxHeap has more elements than the minHeap, dequeue the front of the maxHeap and 
-		// enqueue it in the minHeap
-		// else if the minHeap has more elements than the maxHeap, dequeue the front of the minHeap and
-		// enqueue it in the maxHeap
-		// reason: we want to maintain the size of the maxHeap to be equal to or 1 more than the minHeap
-		if (Math.abs(this.maxHeap.size() - this.minHeap.size()) > 1) {
-			if (this.maxHeap.size() > this.minHeap.size() + 1) {
-				this.minHeap.enqueue(this.maxHeap.dequeue());
-			} else {
-				this.maxHeap.enqueue(this.minHeap.dequeue());
-			}
+		// Balance the heaps to make sure their sizes differ by no more than 1
+		if (this.small.size() > this.large.size() + 1) {
+			this.large.enqueue(this.small.dequeue());
+		} else if (this.large.size() > this.small.size() + 1) {
+			this.small.enqueue(this.large.dequeue());
 		}
 	}
 
 	findMedian(): number {
-		if (this.maxHeap.size() === this.minHeap.size()) {
-			return (this.maxHeap.front() + this.minHeap.front()) / 2;
+		//Complexity: 
+		//Time: O(1) - getting the top of the heap
+		//Space: O(n) - the size of the heap
+
+		//take the top of bigger heap
+		if (this.small.size() > this.large.size()) {
+			return this.small.front();
+		} else if (this.large.size() > this.small.size()) {
+			return this.large.front();
 		} else {
-			const biggerHeap = this.maxHeap.size() > this.minHeap.size() ?
-				this.maxHeap : this.minHeap;
-			return biggerHeap.front();
+			// If both heaps are of the same size, the median is the average of the two heap tops
+			return (this.small.front() + this.large.front()) / 2.0;
 		}
 	}
 }
